66ad74ed8a76f46c53c37cd5c4dee7857f5caf63
emma-catherine <129230522+emma-catherine@users.noreply.github.com>
2023-05-01 11:49:42 -0500

    Add files via upload

---
+++ AKcities.js
@@ -0,0 +1,33 @@
+var AKcity =
+{
+"type": "FeatureCollection",
+"name": "AKcities",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:EPSG::4269" } },
+"features": [
+{ "type": "Feature", "properties": { "Rank": 1, "City": "Anchorage", "Population": "292,545", "Lat": 61.15, "Long": -149.19 }, "geometry": { "type": "Point", "coordinates": [ -149.19, 61.15 ] } },
+{ "type": "Feature", "properties": { "Rank": 2, "City": "Fairbanks", "Population": "33,018", "Lat": 64.84, "Long": -147.65 }, "geometry": { "type": "Point", "coordinates": [ -147.65, 64.84 ] } },
+{ "type": "Feature", "properties": { "Rank": 3, "City": "Juneau", "Population": "32,240", "Lat": 58.45, "Long": -134.23 }, "geometry": { "type": "Point", "coordinates": [ -134.23, 58.45 ] } },
+{ "type": "Feature", "properties": { "Rank": 4, "City": "Badger", "Population": "19,226", "Lat": 64.8, "Long": -147.39 }, "geometry": { "type": "Point", "coordinates": [ -147.39, 64.8 ] } },
+{ "type": "Feature", "properties": { "Rank": 5, "City": "Knik-Fairview", "Population": "18,282", "Lat": 61.5, "Long": -149.65 }, "geometry": { "type": "Point", "coordinates": [ -149.65, 61.5 ] } },
+{ "type": "Feature", "properties": { "Rank": 6, "City": "College", "Population": "11,797", "Lat": 64.87, "Long": -147.82 }, "geometry": { "type": "Point", "coordinates": [ -147.82, 64.87 ] } },
+{ "type": "Feature", "properties": { "Rank": 7, "City": "Tanaina", "Population": "10,314", "Lat": 61.66, "Long": -149.43 }, "geometry": { "type": "Point", "coordinates": [ -149.43, 61.66 ] } },
+{ "type": "Feature", "properties": { "Rank": 8, "City": "North Lakes", "Population": "9,941", "Lat": 69.36, "Long": -149.18 }, "geometry": { "type": "Point", "coordinates": [ -149.18, 69.36 ] } },
+{ "type": "Feature", "properties": { "Rank": 9, "City": "Wasilla", "Population": "9,098", "Lat": 61.58, "Long": -149.47 }, "geometry": { "type": "Point", "coordinates": [ -149.47, 61.58 ] } },
+{ "type": "Feature", "properties": { "Rank": 10, "City": "Sitka", "Population": "8,518", "Lat": 57.18, "Long": -135.31 }, "geometry": { "type": "Point", "coordinates": [ -135.31, 57.18 ] } },
+{ "type": "Feature", "properties": { "Rank": 11, "City": "Kalifornsky", "Population": "8,428", "Lat": 60.44, "Long": -151.2 }, "geometry": { "type": "Point", "coordinates": [ -151.2, 60.44 ] } },
+{ "type": "Feature", "properties": { "Rank": 12, "City": "Meadow Lakes", "Population": "8,367", "Lat": 61.64, "Long": -149.61 }, "geometry": { "type": "Point", "coordinates": [ -149.61, 61.64 ] } },
+{ "type": "Feature", "properties": { "Rank": 13, "City": "Ketchikan", "Population": "8,150", "Lat": 55.36, "Long": -131.67 }, "geometry": { "type": "Point", "coordinates": [ -131.67, 55.36 ] } },
+{ "type": "Feature", "properties": { "Rank": 14, "City": "Kenai", "Population": "7,452", "Lat": 60.56, "Long": -151.21 }, "geometry": { "type": "Point", "coordinates": [ -151.21, 60.56 ] } },
+{ "type": "Feature", "properties": { "Rank": 15, "City": "Steele Creek", "Population": "7,399", "Lat": 64.93, "Long": -147.4 }, "geometry": { "type": "Point", "coordinates": [ -147.4, 64.93 ] } },
+{ "type": "Feature", "properties": { "Rank": 16, "City": "South Lakes", "Population": "6,421", "Lat": 61.35, "Long": -149.18 }, "geometry": { "type": "Point", "coordinates": [ -149.18, 61.35 ] } },
+{ "type": "Feature", "properties": { "Rank": 17, "City": "Gateway", "Population": "6,305", "Lat": 61.57, "Long": -149.24 }, "geometry": { "type": "Point", "coordinates": [ -149.24, 61.57 ] } },
+{ "type": "Feature", "properties": { "Rank": 18, "City": "Bethel", "Population": "6,273", "Lat": 60.79, "Long": -161.79 }, "geometry": { "type": "Point", "coordinates": [ -161.79, 60.79 ] } },
+{ "type": "Feature", "properties": { "Rank": 19, "City": "Sterling", "Population": "6,016", "Lat": 60.54, "Long": -150.81 }, "geometry": { "type": "Point", "coordinates": [ -150.81, 60.54 ] } },
+{ "type": "Feature", "properties": { "Rank": 20, "City": "Palmer", "Population": "5,978", "Lat": 61.6, "Long": -149.11 }, "geometry": { "type": "Point", "coordinates": [ -149.11, 61.6 ] } },
+{ "type": "Feature", "properties": { "Rank": 21, "City": "Kodiak", "Population": "5,682", "Lat": 57.79, "Long": -152.41 }, "geometry": { "type": "Point", "coordinates": [ -152.41, 57.79 ] } },
+{ "type": "Feature", "properties": { "Rank": 22, "City": "Chena Ridge", "Population": "5,643", "Lat": 64.79, "Long": -148.04 }, "geometry": { "type": "Point", "coordinates": [ -148.04, 64.79 ] } },
+{ "type": "Feature", "properties": { "Rank": 23, "City": "Homer", "Population": "5,531", "Lat": 59.64, "Long": -151.51 }, "geometry": { "type": "Point", "coordinates": [ -151.51, 59.64 ] } },
+{ "type": "Feature", "properties": { "Rank": 24, "City": "Utqiaġvik", "Population": "5,123", "Lat": 71.17, "Long": -156.47 }, "geometry": { "type": "Point", "coordinates": [ -156.47, 71.17 ] } },
+{ "type": "Feature", "properties": { "Rank": 25, "City": "Fishhook", "Population": "5,055", "Lat": 61.71, "Long": -149.27 }, "geometry": { "type": "Point", "coordinates": [ -149.27, 61.71 ] } }
+]
+}
